8def2210659b9883cc488d7dba44e3a80b1b2666
Chukwuamaka Nwachukwu <chukwuamakanwachukwu@outlook.com>
2024-08-09 00:08:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from models import db
-import routes  
+from routes import bp as routes_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -9,6 +9,9 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+app.register_blueprint(routes_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
